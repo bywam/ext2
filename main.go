8ee6075a1fb01d8c39bdcd25f7c7ea2e55b78d58
bywam <99722814+bywam@users.noreply.github.com>
2024-07-09 07:27:17 +0200

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // init is called by the Go runtime at application startup.
 func init() {
-	modules.Register("k6/x/compare", new(Ext2))
+	modules.Register("k6/x/ext2", new(Ext2))
 }
 
 // Compare is the type for our custom API.
